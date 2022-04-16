feacf226ab110bfcdafea502279a4a46044ada1c
is41-orlovmd <procmax@mail.ru>
2022-04-16 11:34:30 +0500

    version 1.1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-name', function () {
+    return ('Орлов Максим Денисович');
+});
+Route::get('/my-friend', function () {
+    return ('Александр Роденко Витальевич');
+});
+
+Route::get('/my-friend/{name}', function ($name) {
+    return $name;
+});
+
+Route::get('/my-friend/{sity}', function ($sity) {
+    return $sity;
+});
+
+Route::get('/level/{lvl}', function ($lvl) {
+    if($lvl<=25){
+        return 'Новичек';
+    }
+    if($lvl>=26 && $lvl<=50){
+        return 'Специалист';
+    }
+    if($lvl>=51 && $lvl<=75){
+        return 'Босс';
+    }
+    if($lvl>=76 && $lvl<=98){
+        return 'Старик';
+    }
+    else{
+        return 'Король';
+    }
+});
+
+Route::get('/power/{name}', function ($name) {
+    return $name;
+});
